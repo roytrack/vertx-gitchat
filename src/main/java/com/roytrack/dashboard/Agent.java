7a114bae6a24ad3fbc40a9b6ad6428ad234b7d56
ruanchangming <ruanchangming@huobi.com>
2019-03-03 20:36:25 +0800

    dashboard

---
+++ src/main/java/com/roytrack/dashboard/Agent.java
@@ -0,0 +1,36 @@
+package com.roytrack.dashboard;
+
+import java.lang.management.ManagementFactory;
+import java.util.UUID;
+
+import com.sun.management.OperatingSystemMXBean;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.json.JsonObject;
+
+public class Agent extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    Vertx.clusteredVertx(new VertxOptions(), ar -> {
+      if (ar.succeeded()) {
+        Vertx vertx = ar.result();
+        vertx.deployVerticle(new Agent());
+      } else {
+        ar.cause().printStackTrace();
+      }
+    });
+  }
+
+  @Override
+  public void start() {
+    OperatingSystemMXBean systemMBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+    String pid = UUID.randomUUID().toString();
+    vertx.setPeriodic(1000, id -> {
+      JsonObject metrics = new JsonObject();
+      metrics.put("CPU", systemMBean.getProcessCpuLoad());
+      metrics.put("Mem", systemMBean.getTotalPhysicalMemorySize() - systemMBean.getFreePhysicalMemorySize());
+      vertx.eventBus().publish("metrics", new JsonObject().put(pid, metrics));
+    });
+  }
+}
---
+++ src/main/java/com/roytrack/dashboard/Server.java
@@ -0,0 +1,49 @@
+package com.roytrack.dashboard;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class Server  extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    Vertx.clusteredVertx(new VertxOptions(), ar -> {
+      if (ar.succeeded()) {
+        Vertx vertx = ar.result();
+        vertx.deployVerticle(new Server());
+      } else {
+        ar.cause().printStackTrace();
+      }
+    });
+  }
+
+  private JsonObject dashboard = new JsonObject();
+
+  @Override
+  public void start() {
+
+    Router router = Router.router(vertx);
+
+    // The web server handler
+    router.route().handler(StaticHandler.create().setCachingEnabled(false));
+
+    router.get("/dashboard").handler(ctx -> {
+      ctx.response()
+          .putHeader("Content-Type", "application/json")
+          .end(dashboard.encode());
+    });
+
+    vertx.eventBus().<JsonObject>consumer("metrics").handler(msg -> {
+      JsonObject metrics = msg.body();
+      dashboard.mergeIn(metrics);
+    });
+
+
+    vertx.createHttpServer()
+        .requestHandler(router::accept)
+        .listen(8080);
+  }
+}
